ed8543e68a592206d5030ff724ced78d1d63b6f1
Brian Mitzel <bmitzel@users.noreply.github.com>
2014-09-15 21:46:57 -0700

    Initial commit

---
+++ picking.cpp
@@ -0,0 +1,11 @@
+/* 
+ * Programmer: Brian Mitzel
+ * Email: bmitzel@csu.fullerton.edu
+ * Course: CPSC 486
+ * Assignment: 2 - Picking
+ * Due Date: 9/23/2014
+ *
+ * Filename: picking.cpp
+ *
+ * <description>
+ */
